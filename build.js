667d34a159c55ec8da4588acb5a12bb9ef1e2b18
Nate Holland <nateh@spiceworks.com>
2016-07-20 09:56:08 -0500

    using more compliant syntax

--- build.js
+++ build.js
@@ -4,14 +4,14 @@ const exec = require('child_process').exec;
 const build = exec("handlebars templates/*.hbs -f templates.js && echo \"let Handlebars = require('handlebars');\" | cat - templates.js > temp && mv temp templates.js")
 
 
-build.stdout.on('data', (data) => {
+build.stdout.on('data', function(data) {
   console.log(`stdout: ${data}`);
 });
 
-build.stderr.on('data', (data) => {
+build.stderr.on('data', function(data) {
   console.log(`stderr: ${data}`);
 });
 
-build.on('close', (code) => {
+build.on('close', function(code) {
   console.log(`child process exited with code ${code}`);
 });
