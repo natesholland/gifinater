83eb34c1e07f7fd07ef06b671ced82e5b401515b
Nate Holland <nateh@spiceworks.com>
2016-07-19 21:28:42 -0500

    adding some extra css and copy bit

--- templates.js
+++ templates.js
@@ -6,10 +6,12 @@ templates['image.hbs'] = template({"compiler":[7,">= 4.0.0"],"main":function(con
 
   return "<div class=\"col-xs-6 col-md-4\" >\n  <img src=\""
     + alias4(((helper = (helper = helpers.url || (depth0 != null ? depth0.url : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"url","hash":{},"data":data}) : helper)))
-    + "\" style=\"width:100%;max-width:400px;height:100%;max-height:400px\">\n  <br>\n  "
+    + "\" onclick=copyUrlClipboard(\""
     + alias4(((helper = (helper = helpers.url || (depth0 != null ? depth0.url : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"url","hash":{},"data":data}) : helper)))
-    + "\n  <span class=\"glyphicon glyphicon-copy\" onclick=copyUrlClipboard(\""
+    + "\") style=\"width:100%;max-width:400px;height:100%;max-height:400px\">\n  <br>\n  <a  href='#' style=\"overflow:hidden\" onclick=require('electron').shell.openExternal(\""
     + alias4(((helper = (helper = helpers.url || (depth0 != null ? depth0.url : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"url","hash":{},"data":data}) : helper)))
-    + "\")></span>\n</div>\n";
+    + "\")>\n    "
+    + alias4(((helper = (helper = helpers.url || (depth0 != null ? depth0.url : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"url","hash":{},"data":data}) : helper)))
+    + "\n  </a>\n</div>\n";
 },"useData":true});
 })();
